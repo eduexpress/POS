2dfec1f7c9cecd58735dd37a704f7d037f13a413
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2021-04-29 10:29:07 +0530

    20

--- src/lk/ijse/pos/controller/OrderListFormController.java
+++ src/lk/ijse/pos/controller/OrderListFormController.java
@@ -1,4 +1,22 @@
 package lk.ijse.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class OrderListFormController {
+    public AnchorPane contextOfOrderList;
+
+    public void backToHome(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) contextOfOrderList.
+                getScene().getWindow();
+        stage.setScene(new Scene(
+                FXMLLoader.load(getClass()
+                        .getResource
+                                ("../views/DashBoardForm.fxml"))));
+    }
 }
